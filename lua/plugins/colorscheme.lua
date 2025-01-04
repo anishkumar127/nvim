eeaa964f47f2f9ee0fe9e270f8d2d1f086769003
Anish Kumar <anishbishnoi127@gmail.com>
2025-01-04 22:07:19 +0530

    LazyNvim

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = function(_, opts)
+      opts.transparent = true
+      opts.italic_comments = true
+      opts.borderless_telescope = false -- its remove border in search.
+      -- new adding.
+      -- opts.terminal_colors = true
+      opts.cache = true
+      -- opts.theme = { variant = "auto" }
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "cyberdream",
+    },
+  },
+
+  -- modicator (auto color line number based on vim mode)
+  {
+    "mawkler/modicator.nvim",
+    dependencies = "scottmckendry/cyberdream.nvim",
+    init = function()
+      -- These are required for Modicator to work
+      vim.o.termguicolors = true
+      vim.o.cursorline = true
+      vim.o.number = true
+      vim.o.termguicolors = true
+    end,
+    opts = {},
+  },
+}
---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,5 @@
+return {
+  { "ellisonleao/gruvbox.nvim", enabled = false },
+  { "catppuccin/nvim", enabled = false },
+  { "echasnovski/mini.indentscope", enabled = false },
+}
