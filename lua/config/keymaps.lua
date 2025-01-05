e2e761c77b02c12e44ace30231319963d0996f40
Anish Kumar <anishbishnoi127@gmail.com>
2025-01-05 18:00:19 +0530

    dummy some animation false

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- vim.keymap.set("i", "<C-Space>", "<C-x><C-o>", { noremap = true, silent = true })
+
