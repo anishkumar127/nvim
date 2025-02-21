48e64ada4f51477819211344a920db8e7def75ec
Anish Kumar <anishbishnoi127@gmail.com>
2025-02-21 18:15:09 +0530

    feat: comment out vscode plugin import for cleaner configuration

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -31,7 +31,7 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.util.dot" },
-    { import = "lazyvim.plugins.extras.vscode" },
+    -- { import = "lazyvim.plugins.extras.vscode" },
     -- { import = 'plugins.editor.lazygit' },
     -- { import = 'plugins.editor.gitsigns' },
     -- { import = 'plugins.editor.grug-far' },
