5f59a9f2ebeecf65aba9529d6667bc0d4a64a9a4
Anish Kumar <anishbishnoi127@gmail.com>
2025-03-22 23:48:11 +0530

    feat: disabled noice and comment out j-hi fidget.nvim because it's making weird ui

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -3,6 +3,10 @@ return {
   { "ellisonleao/gruvbox.nvim", enabled = false },
   -- { "catppuccin/nvim", enabled = false },
   { "tokyonight.nvim", enabled = false },
+  {
+    "folke/noice.nvim",
+    enabled = false,
+  },
   -- { "echasnovski/mini.indentscope", enabled = false },
     -- { "echasnovski/mini.pairs", enabled = false },
   -- {"nvim-treesitter/nvim-treesitter-context", enable = false}
